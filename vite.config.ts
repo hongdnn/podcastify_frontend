2fba27183f148ceaff49e2ab2468a85837845bb0
hongdnn <namhongdoan99@gmail.com>
2026-04-14 08:30:06 -0700

    set build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [tailwindcss()],
+    build: {
+        outDir: "build",
+    },
     test: {
         include: ["tests/**/*.test.ts", "tests/**/*.spec.ts"],
     },
